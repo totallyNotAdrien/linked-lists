d5aa8ef0b610dc1d9f8570f3ec03e19ed1336a43
Tina Thompson <mark_junior13@yahoo.com>
2021-09-02 11:47:05 -0500

    finished

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,5 @@
 require_relative "node.rb"
+
 class LinkedList
   attr_reader :head, :tail
 
@@ -41,6 +42,70 @@ class LinkedList
     len
   end
 
+  def at(index)
+    return nil if !@head || index < 0
+    return @head if index == 0
+
+    curr_index = 0
+    curr = @head
+    while curr && curr_index < index
+      curr = curr.next_node
+      curr_index += 1
+    end
+    curr
+  end
+
+  def pop
+    return nil unless @head
+
+    if @head == @tail
+      temp = @head
+      @head = nil
+      @tail = nil
+      temp
+    elsif @head.next_node == @tail
+      temp = @tail
+      @head.next_node = nil
+      @tail = @head
+      temp
+    else
+      curr = @head
+      while curr.next_node != @tail
+        curr = curr.next_node
+      end
+      temp = @tail
+      @tail = curr
+      @tail.next_node = nil
+      temp
+    end
+  end
+
+  def contains?(value)
+    return false unless @head
+    return true if @head.value == value || @tail.value == value
+
+    curr = @head
+    while curr
+      return true if curr.value == value
+      curr = curr.next_node
+    end
+    false
+  end
+
+  def find(value)
+    return nil unless @head
+    return 0 if @head.value == value
+
+    index = 0
+    curr = @head
+    while curr
+      return index if curr.value == value
+      curr = curr.next_node
+      index += 1
+    end
+    nil
+  end
+
   def to_s
     curr = @head
     output = ""
@@ -50,5 +115,61 @@ class LinkedList
     end
     output << "nil"
     output
+  end
+
+  def insert_at(value, index)
+    if index <= 0
+      prepend(value)
+      return
+    end
+
+    if index >= size
+      append(value)
+      return
+    end
+
+    curr_index = 0
+    curr = @head
+    while curr_index < index - 1
+      curr = curr.next_node
+      curr_index += 1
+    end
+
+    node = Node.new(value)
+    node.next_node = curr.next_node
+    curr.next_node = node
+  end
+
+  def remove_at(index)
+    len = size
+    return unless @head
+    return if index < 0 || index >= len
+
+    if index == 0
+      @head = @head.next_node
+      return
+    end
+
+    if index == len - 1
+      pop
+      return
+    end
+
+    curr_index = 0
+    curr = @head
+    while curr_index < index - 1
+      curr = curr.next_node
+      curr_index += 1
+    end
+
+    left = curr
+    to_remove = left.next_node
+    right = to_remove.next_node
+    if right
+      left.next_node = right
+    else
+      @tail = left
+      @tail.next_node = nil
+    end
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,21 +1,60 @@
 require_relative "linked_list.rb"
 
-
 nums = (1..9).to_a
 
-
-
+#--------------------APPEND----------------------------------
 puts "list made with append"
 list = LinkedList.new
 nums.each {|num| list.append(num)}
 puts list.to_s
+
+puts "list2 made with prepend"
+list2 = LinkedList.new
+nums.each {|num| list2.prepend(num)}
+puts list2.to_s
+
 puts "size: #{list.size}"
 puts "head value: #{list.head.value}"
+puts "tail value: #{list.tail.value}"
+
+node = list.at(5)
+puts "value at index 5: #{node ? node.value : "nil"}"
+node = list.at(8)
+puts "value at index 8: #{node ? node.value : "nil"}"
+node = list.at(9)
+puts "value at index 9: #{node ? node.value : "nil"}"
+
+while list.head
+  puts "list after popping #{list.pop.value}:"
+  puts list.to_s
+  puts
+end
+
+list = LinkedList.new
+nums.each {|num| list.append(num)}
+puts "list contains 10? #{list.contains?(10)}"
+puts "list contains 7? #{list.contains?(7)}"
 puts
 
-puts "list made with prepend"
-list = LinkedList.new
-nums.each {|num| list.prepend(num)}
-puts list.to_s
-puts "size: #{list.size}"
-puts "head value: #{list.head.value}"
+puts "index of the value '4': #{list.find(4).inspect}"
+puts "index of the value '14': #{list.find(14).inspect}"
+puts
+
+list.insert_at(14,5)
+puts "insert 14 at index 5: #{list.to_s}"
+
+list.insert_at(1, 13)
+puts "insert 1 at index 13: #{list.to_s}"
+
+list.insert_at(62, 0)
+puts "insert 62 at index 0: #{list.to_s}"
+puts
+
+list.remove_at(0)
+puts "remove index 0: #{list.to_s}"
+
+list.remove_at(list.size-1)
+puts "remove last: #{list.to_s}"
+
+list.remove_at(5)
+puts "remove index 5: #{list.to_s}"
